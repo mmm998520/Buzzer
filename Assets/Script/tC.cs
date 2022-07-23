caaae5e83890ca0a2b417bec6fcf4c07263aa2e8
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2022-07-23 09:27:47 +0800

    velert測試

--- Assets/Script/tC.cs
+++ Assets/Script/tC.cs
@@ -34,8 +34,60 @@ public class tC : MonoBehaviour
         characterSize = transform.Find("Body").localScale;
     }
 
+    double t = 0;
+    double a = 10;
+    double v = 0;
+    double p = 0;
+
+    double ua = 10;
+    double uv = 0;
+    double up = 0;
+
+    double va = 10;
+    double vv = 0;
+    double vp = 0;
+
+    double Va = 10;
+    double Vpv = 0;
+    double Vnv = 0;
+    double Vp = 0;
+
+    double Da = 10;
+    double Dp0 = 0;
+    double Dp1 = 0;
     void Update()
     {
+        t += Time.deltaTime;
+        p = v * t + 0.5d * a * t * t;
+        print(t);
+        print(p);
+
+        uv += ua * Time.deltaTime;
+        up += uv * Time.deltaTime;
+        print(Time.time + " ,v=" + uv + ", p=" + up);
+
+        vv += va * Time.deltaTime;
+        vp += vv * Time.deltaTime + 0.5d * va * Time.deltaTime * Time.deltaTime;
+        print(Time.time + " ,v=" + vv + ", p=" + vp);
+
+        Vpv = Vnv;
+        Vnv += Va * Time.deltaTime;
+        Vp += (Vpv + Vnv) / 2 * Time.deltaTime + 0.5d * Va * Time.deltaTime * Time.deltaTime;
+        print(Time.time + " ,v=" + Vnv + ", p=" + Vp);
+
+        /*
+        Dp0 = Dp1;
+        Dp1 += (Dp1 - Dp0) * Time.deltaTime + 0.5d * Da *Time.deltaTime*Time.deltaTime;
+        print(Time.time + " ,v=" + (Dp1 - Dp0) + ", p=" + Dp1);
+        */
+
+        /*
+        VVpv = VVnv;
+        VVnv += VVa * Time.deltaTime;
+        VVp += VVnv * Time.deltaTime + 1 / 2 * VVa * Time.deltaTime * Time.deltaTime;
+        print(Time.time + " ,v=" + VVnv + ", p=" + VVp);
+        */
+
         getInput();
     }
 
@@ -112,7 +164,7 @@ public class tC : MonoBehaviour
         isGround = Mathf.Min(hitDis) <= isGroundRayLength;
         advanceisGround = Mathf.Min(hitDis) <= advanceisGroundRayLength;
 
-        print(Mathf.Min(hitDis) +","+ isGround);
+        //print(Mathf.Min(hitDis) +","+ isGround);
 
         velocity.y += gravity * Time.fixedDeltaTime;
         if (velocity.y < fallDownSpeedLimit)
